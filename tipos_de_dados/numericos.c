e4ca41e958ccd89052fd2ce6227f4576c3ae6b52
henricolas23 <henricolas23filizola@gmail.com>
2025-03-27 12:45:56 -0300

    add atividade 1

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]){
-int idade = 30;
-printf("a idade eh %d", idade); 
-    return 0;
+int idade;
+float altura;
+ float peso;
+ printf("digite a idade: ");
+ scanf("%d", &idade);
+ printf("digite a altura: ");
+ scanf("%f", &altura);
+ printf("digite o peso: ");
+ scanf("%f",&peso);
+ printf("a idade eh %d\n", idade);
+ printf("a altura eh %.2f\nO pseo eh %.2f", altura, peso);
+
+ double dobro_peso = peso * 2.0;
+ printf("o dobro do peso eh %f", dobro_peso);
+ return 0;
 }
 
