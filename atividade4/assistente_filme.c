7efe3655deac2b1f379cbe605870604e1404403d
henricolas23 <henricolas23filizola@gmail.com>
2025-04-04 13:27:12 -0300

    add atividade

--- atividade4/assistente_filme.c
+++ atividade4/assistente_filme.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-    float leve, inteso, comedia, animacao;
-
-    char opcao;
-    printf("escolha uma opcao: \n");
-    printf("1 - leve\n");
-    printf("2 - intenso\n");
-
-    scanf("%c", &opcao);
-
-     if (opcao == '1') {
-        printf("leve\n");
-        float 1, 2;
-        char opicao;
-        printf("1 - comedia\n");
-        printf("2 - animacao\n");
-
-        scanf("%c",opcao);
-     }
-
+int main(int argc, char* argv []) {
+    int tipo, genero, subgenero, escolha;
+    
+    printf("Escolha o tipo de filme:\n1 - Leve\n2 - Intenso\n");
+    scanf("%d", &tipo);
+    
+    if (tipo == 1) {
+        printf("Escolha o gênero:\n1 - Comédia\n2 - Animação\n");
+        scanf("%d", &genero);
         
+        if (genero == 1) {
+            printf("Recomendamos assistir ao filme: As Branquelas\n");
+        } else {
+            printf("Recomendamos assistir ao filme: Toy Story\n");
         }
-     
-
-
-
-
-
-
-
+    } else {
+        printf("Escolha o gênero:\n1 - Terror\n2 - Ação\n");
+        scanf("%d", &genero);
+        
+        if (genero == 1) {
+            printf("Escolha o subgênero de terror:\n1 - Psicológico\n2 - Sobrenatural\n");
+            scanf("%d", &subgenero);
+            
+            if (subgenero == 1) {
+                printf("Recomendamos assistir ao filme: Corra!\n");
+            } else {
+                printf("Recomendamos assistir ao filme: Invocação do Mal\n");
+            }
+        } else {
+            printf("Escolha o tipo de ação:\n1 - Super-heróis\n2 - Aventura realista\n");
+            scanf("%d", &subgenero);
+            
+            if (subgenero == 1) {
+                printf("Recomendamos assistir ao filme: Vingadores: Ultimato\n");
+            } else {
+                printf("Recomendamos assistir ao filme: Mad Max: Estrada da Fúria\n");
+            }
+        }
+    }
     
-    return 0; 
-    
+    return 0;
 }
